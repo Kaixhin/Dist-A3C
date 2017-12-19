6007fb08691577b9c9607cc19e1adaf806163d8e
Kaixhin <design@kaixhin.com>
2017-12-19 23:06:51 +0000

    Upgrade for 0.3

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from torch import nn
+from torch.nn import functional as F
 
 
 class ActorCritic(nn.Module):
@@ -7,9 +8,6 @@ class ActorCritic(nn.Module):
     super(ActorCritic, self).__init__()
     self.state_size = observation_space.shape[0]
     self.action_size = action_space.n
-
-    self.relu = nn.ReLU(inplace=True)
-    self.softmax = nn.Softmax()
 
     # Pass state into model body
     self.conv1 = nn.Conv2d(3, 16, 8, stride=4, padding=3)
@@ -20,12 +18,12 @@ class ActorCritic(nn.Module):
     self.fc_critic = nn.Linear(hidden_size, 1)
 
   def forward(self, x, h):
-    x = self.relu(self.conv1(x))
-    x = self.relu(self.conv2(x))
+    x = F.relu(self.conv1(x))
+    x = F.relu(self.conv2(x))
     x = x.view(x.size(0), -1)
-    x = self.relu(self.fc1(x))
+    x = F.relu(self.fc1(x))
     h = self.lstm(x, h)  # h is (hidden state, cell state)
     x = h[0]
-    policy = self.softmax(self.fc_actor(x)).clamp(max=1 - 1e-20)  # Prevent 1s and hence NaNs
+    policy = F.softmax(self.fc_actor(x), dim=1).clamp(max=1 - 1e-20)  # Prevent 1s and hence NaNs
     V = self.fc_critic(x)
     return policy, V, (h[0], h[1])
